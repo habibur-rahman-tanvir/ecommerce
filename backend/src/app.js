78c730ee4d740add3f5ac6f227df43bdde0ecc06
habibur-rahman-tanvir <habibur353637@gmail.com>
2025-12-12 20:29:23 +0600

    feat: add CORS middleware for handling cross-origin requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import sellerAuthRoute from './routes/sellerAuthRoute.js';
 import errorHandler from './middleware/errorHandler.js';
+import corsMiddleware from './middleware/cors.js';
 
 const app = express();
 
+app.use(corsMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
---
+++ backend/src/middleware/cors.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import AppError from '../error/AppError.js';
+
+const whiteList = [];
+
+const corsMiddleware = cors({
+  origin: (origin, callbak) => {
+    if (whiteList.length === 0 || whiteList.includes(origin)) {
+      callbak(null, true);
+    } else {
+      callbak(new AppError('Not allowed by CORS'));
+    }
+  },
+});
+
+export default corsMiddleware;
